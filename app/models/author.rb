3ebd8a528af01376dffce89eb619390b07147e71
wachira <wachiraa954@gmail.com>
2022-10-03 20:57:16 +0300

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ApplicationRecord
     validates :name, presence: true
-    validates :email, uniqueness: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,22 @@
 class Post < ApplicationRecord
     validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
     validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
-    validates :content, length: { minimum: 100}
+    validate :title_sufficiently_clickbait
+  
+    TITLE_NOT_CONTAIN = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top/i,
+      /Guess/i
+  
+  ]
+  
+  def title_sufficiently_clickbait
+      if TITLE_NOT_CONTAIN.none? { |word| word.match title}
+          errors.add(:title, "Title not clickbaity")
+      end
+  end
+  
   end
